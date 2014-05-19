6d459a393adf6d034389baa0a4f1f1a4b495082e
mc2cy <mc2cy@virginia.edu>
2014-05-18 23:54:09 -0400

    Update getInfo.php

--- getInfo.php
+++ getInfo.php
@@ -1,7 +1,7 @@
 <?php
 function info($username) {
   
-  $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720mc2cy', 'elchino419', 'cs4720mc2cy');
+  $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720mc2cy', 'pass', 'cs4720mc2cy');
   if (mysqli_connect_errno()) {
       echo "Failed connection!";
   }
